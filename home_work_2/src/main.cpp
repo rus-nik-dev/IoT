92091be9a10a28d05beed3adf430e97c36e65d01
Ruslan Nikitin <ruslan.nikitin@flixbus.com>
2026-08-19 14:14:40 +0300

    ready

--- home_work_2/src/main.cpp
+++ home_work_2/src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+struct SensorData {
+  float temperature;
+  float humidity;
+  unsigned long timestamp;
+};
+
+SensorData sensor;
+
+SensorData generateReading() {
+  SensorData data;
+  data.temperature = 15.0 + (random(0, 1501) / 100.0);
+  data.humidity = 30.0 + (random(0, 3501) / 100.0);
+  data.timestamp = millis();
+  return data;
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void printSensorData(SensorData sensor) {
+  Serial.print("Temperature: ");
+  Serial.print(sensor.temperature, 2);
+
+  Serial.print(" C | Humidity: ");
+  Serial.print(sensor.humidity, 2);
+
+  Serial.print(" % | Timestamp: ");
+  Serial.println(sensor.timestamp);
+}
+
+void printMemoryState() {
+  Serial.print("Free heap: ");
+  Serial.print(ESP.getFreeHeap() / 1024.0, 2);
+  Serial.println(" KB");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for(uint8_t i = 0; i < 3; i++) {
+    sensor = generateReading();
+    printSensorData(sensor);
+    delay(20000);
+  }
+
+  printMemoryState();
 }
